9ccc0ca38321d7998d2958e3a5997f9666d0f45c
나의찬 <cab07036@naver.com>
2026-01-05 22:15:47 +0900

    refactor(Attendance): 출석 시간 LocalDate, LocalTime으로 분리
    출석 시간만 수정해야하는 상황이 생겨 날짜와 시간을 분리하였습니다.

--- src/main/java/attendance/domain/model/Attendance.java
+++ src/main/java/attendance/domain/model/Attendance.java
@@ -1,28 +1,44 @@
 package attendance.domain.model;
 
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Attendance {
 
     private Crew crew;
 
-    private LocalDateTime attendanceAt;
+    private LocalDate attendanceDate;
 
-    private Attendance(Crew crew, LocalDateTime attendanceAt) {
+    private LocalTime attendanceTime;
+
+    // 출석 상태
+    private 
+
+    private Attendance(Crew crew, LocalDate attendanceDate, LocalTime attendanceTime) {
         this.crew = crew;
-        this.attendanceAt = attendanceAt;
+        this.attendanceDate = attendanceDate;
+        this.attendanceTime = attendanceTime;
     }
 
     public static Attendance of(Crew crew, LocalDateTime attendanceAt) {
-        return new Attendance(crew, attendanceAt);
+        return new Attendance(crew, attendanceAt.toLocalDate(), attendanceAt.toLocalTime());
     }
 
     public Crew getCrew() {
         return crew;
     }
 
-    public LocalDateTime getAttendanceAt() {
-        return attendanceAt;
+    public LocalDate getAttendanceDate() {
+        return attendanceDate;
+    }
+
+    public LocalTime getAttendanceTime() {
+        return attendanceTime;
+    }
+
+    public void modifyAttendanceTime(LocalTime attendanceTime) {
+        this.attendanceTime = attendanceTime;
     }
 }
