ea85243736838a9f746f353aa10ca7bd30b55e55
나의찬 <cab07036@naver.com>
2026-01-06 21:50:52 +0900

    refactor(CrewStatus): 크루 제적 상태에 code값 추가
    제적 상태 조회 시에 결과를 정렬할 때 코드를 기준으로 정렬하기 위해서 코드를 추가하였습니다.

--- src/main/java/attendance/domain/model/CrewStatus.java
+++ src/main/java/attendance/domain/model/CrewStatus.java
@@ -2,18 +2,24 @@ package attendance.domain.model;
 
 public enum CrewStatus {
 
-    NORMAL("일반"),
-    WARNING("경고"),
-    INTERVIEW("면담"),
-    DISMISSAL("제적");
+    NORMAL("일반", 1),
+    WARNING("경고", 2),
+    INTERVIEW("면담", 3),
+    DISMISSAL("제적", 4);
 
-    private String name;
+    private final String name;
+    private final int code;
 
-    CrewStatus(String name) {
+    CrewStatus(String name, int code) {
         this.name = name;
+        this.code = code;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getCode() {
+        return code;
     }
 }
